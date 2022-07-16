6e9b23359dab78dd62b564a1dcc152cc60a360c1
RitaNyeinAung <ritanyeinaung.ec@gmail.com>
2022-07-16 16:59:29 +0630

    second commit

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,12 +1,21 @@
 <script setup>
+import { ref } from 'vue'
 import { useCounterStore } from '@/stores/counter.js'
 const storeData = useCounterStore()
+const parentMessage = ref('parentData')
+const items = ref([{ message: 'rita' }, { message: 'nyein' }])
+items.value.forEach((item,index) => {
+  console.log(parentMessage, item.message, index)
+});
 </script>
 
 <template>
   <div class="token_data">
     <h2>This is Token.</h2>
     {{ storeData.token }}
+  </div>
+  <div v-for="(item, index) in items" :key="index">
+    {{ parentMessage }} - {{index}} - {{item.message}}
   </div>
 </template>
 
